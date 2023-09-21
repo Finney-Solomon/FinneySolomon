d43c21e37cd47b3d2b85dfbb63360e5b2447aea2
Finney Solomon <finney.solom.1@gmail.comgit config --global user.email finney.solomon.1@gmail.com>
2023-09-21 07:45:04 -0400

    007:Components Alignment and CSS corrections

--- finney-solomon/src/Pages/AboutMe.jsx
+++ finney-solomon/src/Pages/AboutMe.jsx
@@ -1,61 +1,86 @@
-import React, { useEffect } from 'react'
-import './style.css';
-import Aos from 'aos';
-import 'aos/dist/aos.css'
-
-import { Card, CardBody, CardHeader, Divider } from '@nextui-org/react';
+import React, { useEffect } from "react";
+import "./style.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 export const AboutMe = () => {
-    useEffect(() => {
-        Aos.init();
-        Aos.refresh();
-    }, []);
-    return (
-        <div >
-            <div
-                id="about"
-                className="min-h-screen flex flex-col text-center gap-5  text-lg font-normal"
-            >
-                <div className="head  font-bold" data-aos={"zoom-in"}  data-aos-duration="3000">
-                 <h1> About Me</h1>   
-                </div>
-                <div className="flex flex-row  gap-6 ml-8">
-                    <div className="details flex-col mt-14 flex flex-auto w-64 gap-6">
-                        <h3 className="text-3xl font-medium" data-aos={"fade-left"}  data-aos-duration="3000" data-aos-easing="linear">
-                            I'm <span className="text-red">Finney Solomon,</span> a Full Stack Developer
-                        </h3>
-                        <p
-                            className="pt-5 leading-7 text-slate-900 text-justify"
-                            data-aos={"fade-left"}  data-aos-duration="3000"
-                        >
-                            In my professional career, I've worked on various projects, leveraging a wide range of
-                            technologies, including React, Redux, JavaScript, Figma and etc... and I love exploring new technologies
-                        </p>
-                        <p
-                            className="text-justify leading-7 text-slate-900"
-                            data-aos={"fade-right"} data-aos-duration="3000"
-                        >
-                            When I'm not coding, you can find me playing the keyboard or guitar.
-                            Music has been an integral part of my life, and I find it relaxing and inspiring.
-                            I also love spending quality time with my pets, Finn & Jimmy, who always bring joy and companionship to my life.
-                            In addition to these interests, I'm passionate about traveling and photography.
-                            Exploring new places and capturing the beauty of the world through my camera lens is both fulfilling and enriching.
-                        </p>
-                        <p className="text-justify leading-7 text-slate-900"
-                            data-aos={"fade-left"} data-aos-duration="3000" >
-                            Technology has always been an ever-evolving field, and I'm excited to see what the future holds. I'm
-                            open to collaborations, new opportunities, and contributing to meaningful projects.</p>
-                        <p className="text-justify leading-7 text-slate-900"
-                            data-aos={"fade-right"}  data-aos-duration="3000" >
-                            Let's connect and explore the possibilities together!
-                        </p>
-                    </div>
-                    <div
-                        class="relative flex-auto w-32   sm:rounded-lg  pl-4"
-                        data-aos={"slide-left"}
-                    >
-                    </div>
-                </div>
-            </div>
+  useEffect(() => {
+    Aos.init();
+    Aos.refresh();
+  }, []);
+  return (
+    <div>
+      <div
+        id="about"
+        className="min-h-screen flex flex-col text-center gap-5  text-lg font-normal"
+      >
+        <div
+          className="head  font-bold"
+          data-aos={"zoom-in"}
+          data-aos-duration="3000"
+        >
+          <h1> About Me</h1>
         </div>
-    )
-}
+        <div className="flex flex-row  gap-6 ml-8">
+          <div className="about-details flex-col mt-14 flex flex-auto w-64 gap-6">
+            <h3
+              className="text-3xl font-medium"
+              data-aos={"fade-left"}
+              data-aos-duration="3000"
+              data-aos-easing="linear"
+            >
+              I'm <span className="text-red">Finney Solomon,</span> a Full Stack
+              Developer
+            </h3>
+            <p
+              className="pt-5 leading-7 text-slate-900 text-justify"
+              data-aos={"fade-left"}
+              data-aos-duration="3000"
+            >
+              In my professional career, I've worked on various projects,
+              leveraging a wide range of technologies, including React, Redux,
+              JavaScript, Figma and etc... and I love exploring new technologies
+            </p>
+            <p
+              className="text-justify leading-7 text-slate-900"
+              data-aos={"fade-right"}
+              data-aos-duration="3000"
+            >
+              When I'm not coding, you can find me playing the keyboard or
+              guitar. Music has been an integral part of my life, and I find it
+              relaxing and inspiring. I also love spending quality time with my
+              pets, Finn & Jimmy, who always bring joy and companionship to my
+              life. In addition to these interests, I'm passionate about
+              traveling and photography. Exploring new places and capturing the
+              beauty of the world through my camera lens is both fulfilling and
+              enriching.
+            </p>
+            <p
+              className="text-justify leading-7 text-slate-900"
+              data-aos={"fade-left"}
+              data-aos-duration="3000"
+            >
+              Technology has always been an ever-evolving field, and I'm excited
+              to see what the future holds. I'm open to collaborations, new
+              opportunities, and contributing to meaningful projects.
+            </p>
+            <br />
+            <p
+              className="text-justify leading-7 text-slate-900"
+              data-aos={"fade-right"}
+              data-aos-duration="3000"
+            >
+              Let's connect and explore the possibilities together!
+            </p>
+            <br />
+            <br />
+          </div>
+
+          <div
+            class="relative flex-auto w-32   sm:rounded-lg  pl-4"
+            data-aos={"slide-left"}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- finney-solomon/src/Pages/Skills.jsx
+++ finney-solomon/src/Pages/Skills.jsx
@@ -1,44 +1,58 @@
-import { Card, CardBody, CardFooter, Image, Tab, Tabs } from '@nextui-org/react';
-import React from 'react';
-import './style.css'; // Make sure you have the appropriate CSS file imported
-import mySkill from '../Json/skillsList';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Tab,
+  Tabs,
+} from "@nextui-org/react";
+import React from "react";
+import "./style.css"; // Make sure you have the appropriate CSS file imported
+import mySkill from "../Json/skillsList";
 
 export const Skills = () => {
-    return (
-        <div className="skills-container">
-            <h2 className="skills-heading" >Skills</h2>
-            <p  className="skills-p" >"Committed to growth, I eagerly embrace new challenges. I'll learn and return prepared"</p>
-            <br />
-            <div className="skills-tab-container">
-                <Tabs aria-label="Dynamic tabs" items={mySkill} color={"primary"} >
-                    {(item) => (
-                        <Tab key={item.id} title={item.label}  >
-                            <Card className="skills-card">
-                                <CardBody>
-                                    <div className="skills-grid">
-                                        {item.list.map((skill, index) => (
-                                            <div key={index} className="skill-card-container">
-                                                <Card shadow="sm" border="5" key={index} isPressable className="skill-card">                                                  
-                                                        <Image
-                                                            alt={skill.photo}
-                                                            className="skill-image"
-                                                            src={skill.photo}      
-                                                            fallbackSrc={skill.photo}   
-                                                            srcSet={skill.photo}                            
-                                                        /> 
-                                                        <h4 className="cardFooter">{skill.label}</h4>
-                                                        
-                                                        
-                                                </Card>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        </Tab>
-                    )}
-                </Tabs>
-            </div>
-        </div>
-    );
+  return (
+    <div className="skills-container">
+      <h2 className="heading">Skills</h2>
+      <p className="heading-details">
+        "Committed to growth, I eagerly embrace new challenges. I'll learn and
+        return prepared"
+      </p>
+      <br />
+      <div className="skills-tab-container">
+        <Tabs aria-label="Dynamic tabs" items={mySkill} color={"primary"}>
+          {(item) => (
+            <Tab key={item.id} title={item.label}>
+              <Card className="skills-card">
+                <CardBody>
+                  <div className="skills-grid">
+                    {item.list.map((skill, index) => (
+                      <div key={index} className="skill-card-container">
+                        <Card
+                          shadow="sm"
+                          border="5"
+                          key={index}
+                          isPressable
+                          className="skill-card"
+                        >
+                          <Image
+                            alt={skill.photo}
+                            className="skill-image"
+                            src={skill.photo}
+                            fallbackSrc={skill.photo}
+                            srcSet={skill.photo}
+                          />
+                          <h4 className="cardFooter">{skill.label}</h4>
+                        </Card>
+                      </div>
+                    ))}
+                  </div>
+                </CardBody>
+              </Card>
+            </Tab>
+          )}
+        </Tabs>
+      </div>
+    </div>
+  );
 };
