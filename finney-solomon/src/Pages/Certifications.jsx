ea3c7cadb7ff0cc69f9cfc24ea332ca1b78819cd
Finney Solomon <finney.solomon.1@gmail.com>
2024-01-13 01:25:00 +0530

    015: Added Projects & Certificates, Components Corrections.

--- finney-solomon/src/Pages/Certifications.jsx
+++ finney-solomon/src/Pages/Certifications.jsx
@@ -13,13 +13,13 @@ export const Certifications = () => {
     },
     {
       title: "Software Engineer Intern Certificate",
-      imageUrl: "https://lh3.googleusercontent.com/pw/ABLVV85v_nxTxsBdjxJrMGNwTexZ2GL092hjhG2uDFoWB3TFLbpqwFJkYL9l3pAK_YA6_vob1O1cELXzS0mWqeTwLiGBd5738MT6r6O79MBqrrqc-pb26yU6g9DnLsOwi9cplQR0VokCX1jM0ZDbcWxtgqZ-60Trq5XAa8Jx-2mD9Xd8nkebTOm0CX3dbZLOY1nUVwhlilGGqA0rzE8TEHqmmsCGVdxPn-4xxXGgxOuI01d2kF0yH6IuibTqYQ_5iZvZspIY0E0k6c2hP5qbkYecoU326cfZy1-FYgJmTS9hByK04aI7BK5RvU7I-kct4f8SnDKLC3CaHS0iEr-sA3kcljFWsN2hqbRdm71Ot70ry0wHU14t3R1p0du7PacSlV2s9D0RYe0dzFiNKhdUito9DTzs2k43bFkJ_mbPIUlVH9KNeyn7L82s7_WQCQDiqt5EpUcgHDpRye16mH52M7YHx8DLgBpC0CkR2eCTFo9pt2viMVgiIPxizrKJTXrdo2JpIzTeAX5mJAWyBbt-g4ld7JcM4-Qq8WIAR9rTH5HJmxr4MbeXQHan-bsYH8HilnN-KSkXNFZructWKyMgD8Qz39-La876lF6sSyErOLIE9c65geMCbcF0nis8RXPZ8Svi4VNLMSDzMHTWHNSV2zLqlevc_eJXmtggrc6mkqQBaQ0ard3z5AwiQlSbpDmdNy3GZd2zUzqg8Nv_inQ8TN76RK5rSjOtV3_qGB02HoQf2_N7kGHccXbUYBb1iX47qSZm6_ecEY7lh55oqkgr41Nhx5Q2ohUXIDI7dwz6Wq12rDHKtSL8R3Lgs5vA_8gmIbczCTRQ4FZg_Oj-p8ir_Fp1nTowTgkYtexn5AbvZDp4R9BU5nZr2Ib2XrloJ_5dDkg1bn0=w1059-h802-s-no-gm?authuser=0https://lh3.googleusercontent.com/pw/ABLVV85v_nxTxsBdjxJrMGNwTexZ2GL092hjhG2uDFoWB3TFLbpqwFJkYL9l3pAK_YA6_vob1O1cELXzS0mWqeTwLiGBd5738MT6r6O79MBqrrqc-pb26yU6g9DnLsOwi9cplQR0VokCX1jM0ZDbcWxtgqZ-60Trq5XAa8Jx-2mD9Xd8nkebTOm0CX3dbZLOY1nUVwhlilGGqA0rzE8TEHqmmsCGVdxPn-4xxXGgxOuI01d2kF0yH6IuibTqYQ_5iZvZspIY0E0k6c2hP5qbkYecoU326cfZy1-FYgJmTS9hByK04aI7BK5RvU7I-kct4f8SnDKLC3CaHS0iEr-sA3kcljFWsN2hqbRdm71Ot70ry0wHU14t3R1p0du7PacSlV2s9D0RYe0dzFiNKhdUito9DTzs2k43bFkJ_mbPIUlVH9KNeyn7L82s7_WQCQDiqt5EpUcgHDpRye16mH52M7YHx8DLgBpC0CkR2eCTFo9pt2viMVgiIPxizrKJTXrdo2JpIzTeAX5mJAWyBbt-g4ld7JcM4-Qq8WIAR9rTH5HJmxr4MbeXQHan-bsYH8HilnN-KSkXNFZructWKyMgD8Qz39-La876lF6sSyErOLIE9c65geMCbcF0nis8RXPZ8Svi4VNLMSDzMHTWHNSV2zLqlevc_eJXmtggrc6mkqQBaQ0ard3z5AwiQlSbpDmdNy3GZd2zUzqg8Nv_inQ8TN76RK5rSjOtV3_qGB02HoQf2_N7kGHccXbUYBb1iX47qSZm6_ecEY7lh55oqkgr41Nhx5Q2ohUXIDI7dwz6Wq12rDHKtSL8R3Lgs5vA_8gmIbczCTRQ4FZg_Oj-p8ir_Fp1nTowTgkYtexn5AbvZDp4R9BU5nZr2Ib2XrloJ_5dDkg1bn0=s250-k-rw-no",
+      imageUrl: require("../images/softwareEngineerInternCertificate.jpg"),
 
       link: "https://www.hackerrank.com/certificates/a2a9d84501ff",
     },
     {
       title: "React (Basic) Certificate",
-      imageUrl: "https://lh3.googleusercontent.com/pw/ABLVV8634DgdA0YUvpn2DK3Ak0BC-BFjl_ZlrTfLLhPixsbboS0-bn6Pkq4nUyTrhKhWVx4XKa2lp_xfEeDO6uspCnU3qvo9epy8bMqUmHsLm-hPRMMUBgh2xD57O_mnizMxe-QIf6LYdL-f6FsWvVTegs78qttk0fO7GE02jYprRyyrtTlzlH0H2S0NNSCcUWUqrfYDXPE0d3dz993TJ2je0JL_Y7G2XfTLYAfpVPWo7X5MoAIa3B-YHd8DKGitxAEKjQWy-ir97fyTi6qZOeyffaAT3ytN0PF0VOYPnLZPxrZxFPptc1r73S4TvOv-gX9zsFyf2SddlSp4dPXr3vKA9NVmLi9WGWpvB_oybp_SyFoEV3-JymlOdWzQJpKISpecAVfKdMZSkqwgZOjFlDB_U6qRbJG8dgHomA1P_1xSdXmGRwYVH1EcJuNnFGy9uFU09VbGcFIC48ADeyqkPWPx7x-dHRKysaJrnAyx13Gu533iGRSivQrrFVsefBriQRymLXEqbBTO6vpBrXO9ZtfBDzbE88KfAmWl8JxwYqEuzX3vVQqWgjkM1iyqkBh4z7M1N6rcIVcw5zkCKwDfUxM9aCK32GQQIvi_wYqa7D9uy08W1yrYoWsYTU-BaFBEfJfuwccb3_9gLZePSroBF2binyhke-uTEDhzXYI5SkyBXU1ESXoxfF01H3XNz86iHdShd3V6EdxIvYwbV4G_88s9tC3Kp233nJGFGHFrxiPu79KxDTFhldwOjaiPGkU3qKtkeo83sm1WyD58CXuzjxmVODGO9-1JUT2QmxJPUUp0hIGnN3020OiR0pNoHxxRbf9AyCPFK7NLhjTHVs6_yZEeEJ4-9e2vrIONmmIFpl9Uw3jhjUYKKEt144ZWCjq66Bsmsa8=w1059-h802-s-no-gm?authuser=0https://lh3.googleusercontent.com/pw/ABLVV85v_nxTxsBdjxJrMGNwTexZ2GL092hjhG2uDFoWB3TFLbpqwFJkYL9l3pAK_YA6_vob1O1cELXzS0mWqeTwLiGBd5738MT6r6O79MBqrrqc-pb26yU6g9DnLsOwi9cplQR0VokCX1jM0ZDbcWxtgqZ-60Trq5XAa8Jx-2mD9Xd8nkebTOm0CX3dbZLOY1nUVwhlilGGqA0rzE8TEHqmmsCGVdxPn-4xxXGgxOuI01d2kF0yH6IuibTqYQ_5iZvZspIY0E0k6c2hP5qbkYecoU326cfZy1-FYgJmTS9hByK04aI7BK5RvU7I-kct4f8SnDKLC3CaHS0iEr-sA3kcljFWsN2hqbRdm71Ot70ry0wHU14t3R1p0du7PacSlV2s9D0RYe0dzFiNKhdUito9DTzs2k43bFkJ_mbPIUlVH9KNeyn7L82s7_WQCQDiqt5EpUcgHDpRye16mH52M7YHx8DLgBpC0CkR2eCTFo9pt2viMVgiIPxizrKJTXrdo2JpIzTeAX5mJAWyBbt-g4ld7JcM4-Qq8WIAR9rTH5HJmxr4MbeXQHan-bsYH8HilnN-KSkXNFZructWKyMgD8Qz39-La876lF6sSyErOLIE9c65geMCbcF0nis8RXPZ8Svi4VNLMSDzMHTWHNSV2zLqlevc_eJXmtggrc6mkqQBaQ0ard3z5AwiQlSbpDmdNy3GZd2zUzqg8Nv_inQ8TN76RK5rSjOtV3_qGB02HoQf2_N7kGHccXbUYBb1iX47qSZm6_ecEY7lh55oqkgr41Nhx5Q2ohUXIDI7dwz6Wq12rDHKtSL8R3Lgs5vA_8gmIbczCTRQ4FZg_Oj-p8ir_Fp1nTowTgkYtexn5AbvZDp4R9BU5nZr2Ib2XrloJ_5dDkg1bn0=w1059-h802-s-no-gm?authuser=0https://lh3.googleusercontent.com/pw/ABLVV85v_nxTxsBdjxJrMGNwTexZ2GL092hjhG2uDFoWB3TFLbpqwFJkYL9l3pAK_YA6_vob1O1cELXzS0mWqeTwLiGBd5738MT6r6O79MBqrrqc-pb26yU6g9DnLsOwi9cplQR0VokCX1jM0ZDbcWxtgqZ-60Trq5XAa8Jx-2mD9Xd8nkebTOm0CX3dbZLOY1nUVwhlilGGqA0rzE8TEHqmmsCGVdxPn-4xxXGgxOuI01d2kF0yH6IuibTqYQ_5iZvZspIY0E0k6c2hP5qbkYecoU326cfZy1-FYgJmTS9hByK04aI7BK5RvU7I-kct4f8SnDKLC3CaHS0iEr-sA3kcljFWsN2hqbRdm71Ot70ry0wHU14t3R1p0du7PacSlV2s9D0RYe0dzFiNKhdUito9DTzs2k43bFkJ_mbPIUlVH9KNeyn7L82s7_WQCQDiqt5EpUcgHDpRye16mH52M7YHx8DLgBpC0CkR2eCTFo9pt2viMVgiIPxizrKJTXrdo2JpIzTeAX5mJAWyBbt-g4ld7JcM4-Qq8WIAR9rTH5HJmxr4MbeXQHan-bsYH8HilnN-KSkXNFZructWKyMgD8Qz39-La876lF6sSyErOLIE9c65geMCbcF0nis8RXPZ8Svi4VNLMSDzMHTWHNSV2zLqlevc_eJXmtggrc6mkqQBaQ0ard3z5AwiQlSbpDmdNy3GZd2zUzqg8Nv_inQ8TN76RK5rSjOtV3_qGB02HoQf2_N7kGHccXbUYBb1iX47qSZm6_ecEY7lh55oqkgr41Nhx5Q2ohUXIDI7dwz6Wq12rDHKtSL8R3Lgs5vA_8gmIbczCTRQ4FZg_Oj-p8ir_Fp1nTowTgkYtexn5AbvZDp4R9BU5nZr2Ib2XrloJ_5dDkg1bn0=s250-k-rw-no",
+      imageUrl: require("../images/react(Basic)Certificate.jpg"),
 
       link: "https://www.hackerrank.com/certificates/27673d931443",
     },
@@ -40,7 +40,7 @@ export const Certifications = () => {
       <h2 className="heading">Certification</h2>
       <p className="heading-details">{certificates[currentImageIndex].title}</p>
       <br />
-      <Card className="certification-card">
+      <Card className="certification-card" style={{width:'65vw',height:'70vh'}}>
         <CardBody className="overflow-visible p-0">
           <Image
             shadow="sm"
@@ -48,6 +48,7 @@ export const Certifications = () => {
             classNames="certificate.image"
             onClick={() => window.open(certificates[currentImageIndex].link)}
             src={certificates[currentImageIndex].imageUrl}
+            style={{width:'60vw',height:'60vh'}}
           />
         </CardBody>
       </Card>
@@ -55,9 +56,9 @@ export const Certifications = () => {
         <span onClick={() => handleNextClick(-1)}>
           <ArrowBackIosIcon /> Back{" "}
         </span>
-        <span> {" "}{" "}</span>
+        
         <span onClick={() => handleNextClick(1)}>
-          {" "}   {" "}
+          
           Next <ArrowForwardIosIcon />
         </span>
       </div>
--- finney-solomon/src/Pages/Projects.jsx
+++ finney-solomon/src/Pages/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, CardBody, Image, Button, Code } from "@nextui-org/react";
 import Todo from "../images/Todo.gif";
-
+import singPraises from "../images/singPraises.gif";
 export const Projects = () => {
   const [liked, setLiked] = React.useState(false);
 
@@ -14,6 +14,7 @@ export const Projects = () => {
         justifyContent: "center",
         margin: "auto",
         marginTop: "10vh",
+        backgroundColor:"#fffff"
       }}
       className=" gap-2 grid grid-cols-12 grid-rows-2 px-8"
     >
@@ -22,8 +23,9 @@ export const Projects = () => {
       <br />
       <Card
         isBlurred
-        className="border-none bg-background/60 dark:bg-default-100/50 max-w-[810px]"
+        className="border-none	background-color: rgb(107 114 128); max-w-[810px]"
         shadow="sm"
+    style={{backgroundColor:"#35393dc2"}}
       >
         <CardBody>
           <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4  flex flex-row">
@@ -31,20 +33,107 @@ export const Projects = () => {
               className="relative col-span-6 md:col-span-6"
               style={{ width: "38vw" }}
             >
+              <br />
               <Image
                 alt="Album cover"
                 className="object-contain"
                 width="100%"
                 height={100}
                 shadow="md"
-                src={Todo}
+                src={singPraises}
               />
             </div>
 
-            <div className="flex flex-col col-span-6 md:col-span-6 gap-6 md:gap-4" style={{
-              width: "38vw", alignItems: "center", display: "flex",
-              justifyContent: "flex-start", padding: "2em"
-            }}>
+            <div
+              className="flex flex-col col-span-6 md:col-span-6 gap-6 md:gap-4"
+              style={{
+                width: "38vw",
+                height: "50vh",
+                alignItems: "center",
+                display: "flex",
+                justifyContent: "flex-start",
+                padding: "1em",
+                backgroundColor:"#413d3df"
+              }}
+            >
+              <h1 style={{ fontSize: "2em" }}>Sing Praises</h1>
+              <p style={{ fontSize: "1.2em" }}>
+                Sing Praises is a multilingual lyrics web app, hosted on
+                Firebase and powered by Firebase Cloud. Featuring songs in
+                English, Telugu, Hindi, and Tamil, Users can seamlessly switch
+                between languages and access original song links to watch their
+                favorite tracks.
+              </p>
+              <div
+                style={{
+                  padding: "1em",
+                  display: "flex",
+                  width: "30vw",
+                  margin: "5px",
+                }}
+              >
+                <Code>React</Code>
+                <Code>Firebase</Code>
+                <Code>Firebase Cloud</Code>
+                <Code>JavaScript</Code>
+                <Code>Material UI</Code>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  width: "20em",
+                }}
+              >
+                <Button
+                  color="primary"
+                  onClick={() =>
+                    window.open(
+                      "https://solo-owuuwj.web.app/",
+                      "_blank"
+                    )
+                  }
+                >
+                  Website
+                </Button>
+                <br />
+                <Button
+                  color="primary"
+                  style={{ display: "flex" }}
+                  onClick={() =>
+                    window.open(
+                      "https://github.com/Finney-Solomon/praises-song-book.git",
+                      "_blank"
+                    )
+                  }
+                >
+                  Git
+                </Button>
+              </div>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+      <br />
+      <Card
+        isBlurred
+        className="border-none bg-background/60 dark:bg-default-100/50 max-w-[810px]"
+        shadow="sm"
+        style={{backgroundColor:"#35393dc2"}}
+      >
+        <CardBody>
+          <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4  flex flex-row">
+            <div
+              className="flex flex-col col-span-6 md:col-span-6 gap-6 md:gap-4"
+              style={{
+                width: "38vw",
+                alignItems: "center",
+                display: "flex",
+                justifyContent: "flex-start",
+                padding: "1em",
+              }}
+            >
               <h1 style={{ fontSize: "2em" }}>ToDo's</h1>
               <p style={{ fontSize: "1.2em" }}>
                 Developed a robust full-stack application leveraging the MERN
@@ -53,17 +142,39 @@ export const Projects = () => {
                 while ensuring secure authentication through a comprehensive
                 login and registration system.
               </p>
-              <div style={{ padding: "2em", display: "flex", width: "30vw" }}>
+              <div style={{ padding: "1em", display: "flex", width: "30vw" }}>
                 <Code>React</Code>
                 <Code>NextUI</Code>
                 <Code>JavaScript</Code>
                 <Code>MongoDB</Code>
                 <Code>Express JS</Code>
               </div>
-              <Button color="primary" style={{ display: "flex" }} onClick={() => window.open("https://github.com/Finney-Solomon/ToDo_App_MERN_Stack.git", "_blank")}>
+              <Button
+                color="primary"
+                style={{ display: "flex" }}
+                onClick={() =>
+                  window.open(
+                    "https://github.com/Finney-Solomon/ToDo_App_MERN_Stack.git",
+                    "_blank"
+                  )
+                }
+              >
                 Git
               </Button>
-
+            </div>
+            <div
+              className="relative col-span-6 md:col-span-6"
+              style={{ width: "38vw" }}
+            >
+              <br />
+              <Image
+                alt="Album cover"
+                className="object-contain"
+                width="100%"
+                height={100}
+                shadow="md"
+                src={Todo}
+              />
             </div>
           </div>
         </CardBody>
